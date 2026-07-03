8a77e7d7b6bb2051b19ffee42cf8e9f1aaffea6c
Itallo Vasconcelos <itallo.prog@gmail.com>
2026-07-02 22:35:35 -0300

    chore: configure Tailwind v4, path aliases and add new structural files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import './App.css'
+import { LeadCard } from "./components/leads/LeadCard/LeadCard"
+const mockData = 
+    {
+    id: "1", 
+    initials: "L1",
+    name: "Lead 1", 
+    company: "Company A", 
+    estimatedValue: 10000, 
+    status: "NEW", 
+    temperature: "COLD", 
+    lastContactAt: "2023-09-01"
+}
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className='bg-brand-700 text-white p-4 rounded-lg'> Aqui jaz um teste
+    <LeadCard lead={mockData} />
+    </div>
   )
 }
 
---
+++ src/hooks/useLeads.ts
@@ -0,0 +1,13 @@
+// import { useState, useEffect } from 'react';
+
+
+// export function useLeads() {
+
+//     const [leads, setLeads] = useState<Lead[]>([])
+
+//     useEffect(() => {
+//         const timer = setTimeout(() => {
+//             setLeads([
+//         }
+//     }, [])
+// }
