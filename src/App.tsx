dfa6ec98c8cf8811ee82ae4f44d5cf86ab9ca0d1
Itallo Vasconcelos <itallo.prog@gmail.com>
2026-07-04 22:09:01 -0300

    fix: change mockdata to data

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import { LeadCard } from "./components/leads/LeadCard/LeadCard"
-const mockData = 
-    {
-    id: "1", 
-    initials: "L1",
-    name: "Lead 1", 
-    company: "Company A", 
-    estimatedValue: 10000, 
-    status: "NEW", 
-    temperature: "COLD", 
-    lastContactAt: "2023-09-01"
-}
-
+import leadsData from "../mock/leads.mock.json"
+import type { Lead } from "./types/lead"
 function App() {
+
+  const leads = leadsData as Lead[];
   return (
     <div className='bg-brand-700 text-white p-4 rounded-lg'> Aqui jaz um teste
-    <LeadCard lead={mockData} />
+    <LeadCard lead={leads[2]} variant = "card" />
     </div>
   )
 }
