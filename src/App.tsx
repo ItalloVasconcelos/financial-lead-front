7c9f800fb51b6b23006caed2e31e0fe2e9e0bdc6
Itallo Vasconcelos <itallo.prog@gmail.com>
2026-07-22 20:46:45 -0300

    refactor: Move app to dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,6 @@
-import { LeadCard } from "./components/leads/LeadCard/LeadCard"
-import { useLeads } from './hooks/useLeads'
-function App() {
-  const leadsState = useLeads()
-  switch (leadsState.status) {
-    case "loading":
-      return <h1>"Loading"</h1>
-    case "error":
-      return leadsState.message
-    case "empty":
-      return <h1>0</h1>
-    case "success":
-      return (
-        <div className='bg-brand-700 text-white p-4 rounded-lg'>
-          {
-            leadsState.data.map((leads) =>
-              <LeadCard key={leads.id} lead={leads} variant='card'/>)
-          }
-        </div>
-      )
-  }
 
+function App() {
+  
 }
 
 export default App
